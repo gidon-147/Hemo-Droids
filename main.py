b5aadc8a3dd113f5a8f5a3b842bc0b2509c5e27e
gidon-147 <nubigenaeqn@hotmail.com>
2026-01-26 23:25:41 +0100

    first lesson

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame # type: ignore[import]
+from constants import *
+from logger import log_state
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    
+    # SCREEN SETTINGS
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            pass
+        screen.fill("black")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
